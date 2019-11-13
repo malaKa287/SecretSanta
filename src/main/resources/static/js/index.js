72566d410b1460c1f9311af642e221932cefd21b
Ruslan_Lelyk <ruslan_lelyk@epam.com>
2019-11-13 16:22:23 +0200

    check at home 1

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -29,3 +29,27 @@ function remove() {
   }
 }
 
+$(document).ready(function(){
+    //the click function does not seem to be working in the example, so I have replaced it with this.
+    $(document).on('click', '.icon-delete', function(){
+        $(this).parent().remove();
+    });
+    //Keep a single clone of the original
+    var clonedField = $('.ingredient_field').clone(),
+    main = $('#dynamic_ingredients');
+
+    // Add in the delete <a>
+    $('<a>', {
+        text: 'Remove Step',
+        class: 'icon-delete',
+        href: '#'
+    }).appendTo(clonedField);
+
+    // Clone the cloned original and append it back to the list
+    $('#add-ingredient').click(function() {
+        main.append(clonedField.clone());
+        return false;
+    });
+});
+
+
