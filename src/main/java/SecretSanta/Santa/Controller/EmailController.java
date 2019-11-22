fadb130b8725f7e91c3f7c88751b096680f13a3b
Ruslan_Lelyk <ruslan_lelyk@epam.com>
2019-11-22 15:50:46 +0200

    clear gmail folders

--- src/main/java/SecretSanta/Santa/Controller/EmailController.java
+++ src/main/java/SecretSanta/Santa/Controller/EmailController.java
@@ -8,8 +8,9 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.mail.MessagingException;
+import javax.mail.*;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.*;
 
 @Controller
@@ -30,33 +31,33 @@ public class EmailController {
 
 //        for (int i = 0; i < 10; i++) {
             List<UserData> shuffledList = shuffleCollection(userDataList);
-            System.out.println(shuffledList);
+            System.out.println("shuffledList: " + shuffledList);
 
-            Map<String, String> pairedMap = pairMap(userDataList);
-            System.out.println("paired map: " + pairedMap);
+//            Map<String, String> pairedMap = pairMap(userDataList);
+//            System.out.println("paired map: " + pairedMap);
 //        }
 
 
-//        sendMail(userDataList);
+        sendMail(shuffledList);
 
         return "santa";
     }
 
 
-    public void sendMail(List<UserData> userDataList) {
+    public void sendMail(List<UserData> shuffledList) {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = null;
 
-        for (int i = 0; i < userDataList.size(); i++) {
+        for (int i = 0; i < shuffledList.size(); i++) {
             String emailTo;
             String recipientName;
 
-            if (i == (userDataList.size() - 1)) {
-                emailTo = userDataList.get(i).getEmail();
-                recipientName = userDataList.get(0).getName();
+            if (i == (shuffledList.size() - 1)) {
+                emailTo = shuffledList.get(i).getEmail();
+                recipientName = shuffledList.get(0).getName();
             } else {
-                emailTo = userDataList.get(i).getEmail();
-                recipientName = userDataList.get(i + 1).getName();
+                emailTo = shuffledList.get(i).getEmail();
+                recipientName = shuffledList.get(i + 1).getName();
             }
 
             try {
@@ -68,11 +69,15 @@ public class EmailController {
 //                helper.addAttachment("file.xlsx", fileSystemResource, "application/octet-stream;");
 
                 javaMailSender.send(message);
+
+
             } catch (MessagingException e) {
                 e.printStackTrace();
                 System.out.println("exception sendMail()");
             }
         }
+        // clear gmail folders
+        deleteSentEmail();
     }
 
 
@@ -104,21 +109,67 @@ public class EmailController {
         return userDataList;
     }
 
-    public Map<String, String> pairMap(List<UserData> userDataList){
-        Map<String, String> pairedMap = new HashMap<>();
-        String senderEmail;
-        String recipientEmail;
-        for (int i = 0; i < userDataList.size(); i++){
-            if (i == (userDataList.size() - 1)) {
-                senderEmail = userDataList.get(i).getEmail();
-                recipientEmail = userDataList.get(0).getEmail();
-            } else {
-                senderEmail = userDataList.get(i).getEmail();
-                recipientEmail = userDataList.get(i + 1).getEmail();
+    public void deleteSentEmail() {
+        try {
+            Properties properties = new Properties();
+            properties.put("mail.imap.host", "smtp.gmail.com");
+            properties.put("mail.imap.port", "587");
+            properties.put("mail.imap.starttls.enable", "true");
+
+            Session session = Session.getInstance(properties);
+            Store store = session.getStore("imaps");
+            store.connect("smtp.gmail.com", "Secret.Santa.EPAM2019@gmail.com", "asdasdaw12");
+
+            Folder folder = store.getFolder("[Gmail]/Надіслані");
+            Folder folderBucket = store.getFolder("[Gmail]/Кошик");
+//            Folder allEmails = store.getFolder("[Gmail]/Уся пошта");
+
+            folder.open(Folder.READ_WRITE);
+            folderBucket.open(Folder.READ_WRITE);
+//            allEmails.open(Folder.READ_WRITE);
+
+            Message[] messages = folder.getMessages();
+            Message[] messagesBucket = folderBucket.getMessages();
+//            Message[] allMessages = allEmails.getMessages();
+
+            for (Message messageSent: messages) {
+                messageSent.setFlag(Flags.Flag.DELETED, true);
             }
-            pairedMap.put(senderEmail, recipientEmail);
+            for (Message messageBucket: messagesBucket) {
+                messageBucket.setFlag(Flags.Flag.DELETED, true);
+            }
+//            for (Message messageAll: allMessages) {
+//                messageAll.setFlag(Flags.Flag.DELETED, true);
+//            }
+
+            folder.close(true);
+            folderBucket.close(true);
+//            allEmails.close(true);
+            store.close();
+
+            System.out.println("Gmail account is cleared");
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
-        return pairedMap;
     }
+
+//    public Map<String, String> pairMap(List<UserData> userDataList){
+//        Map<String, String> pairedMap = new HashMap<>();
+//        String senderEmail;
+//        String recipientEmail;
+//        for (int i = 0; i < userDataList.size(); i++){
+//            if (i == (userDataList.size() - 1)) {
+//                senderEmail = userDataList.get(i).getEmail();
+//                recipientEmail = userDataList.get(0).getEmail();
+//            } else {
+//                senderEmail = userDataList.get(i).getEmail();
+//                recipientEmail = userDataList.get(i + 1).getEmail();
+//            }
+//            pairedMap.put(senderEmail, recipientEmail);
+//        }
+//        return pairedMap;
+//    }
+
+
 
 }
